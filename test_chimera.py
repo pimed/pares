521c67bac34566eca2726726206f2fe6c238f5e8
Mirabela Rusu <mirabela.rusu@gmail.com>
2026-08-17 07:10:01 -0700

    license

--- test_chimera.py
+++ test_chimera.py
@@ -1,3 +1,9 @@
+##
+#Copyright (c) 2026 Mirabela Rusu, Radiology, Stanford University
+#This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. 
+#To view a copy of this license, visit http://creativecommons.org or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+##
+
 from test_one_case import run_one_case
 import argparse
 import csv
--- test_one_case.py
+++ test_one_case.py
@@ -1,3 +1,8 @@
+##
+#Copyright (c) 2026 Mirabela Rusu, Radiology, Stanford University
+#This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. 
+#To view a copy of this license, visit http://creativecommons.org or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+##
 import os
 import SimpleITK as sitk
 import argparse
